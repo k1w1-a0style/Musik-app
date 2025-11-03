47bdac9089b8b519c52cfd1a2dcaf36cc6f8ff8b
k1w1-a0style <h91874350@gmail.com>
2025-11-03 04:49:32 +0100

    Update player.tsx

--- player.tsx
+++ player.tsx
@@ -1,1 +1,10 @@
-// Player-Code
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+export function PlayerScreen() {
+  return (
+    <View>
+      <Text>Player Screen</Text>
+      <Button title='Play' onPress={() => console.log('Play')} />
+    </View>
+  );
+}
