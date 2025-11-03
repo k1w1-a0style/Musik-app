0d6966cdc6ae9f2392aeaab1ce12b25ee6958a1d
k1w1-a0style <h91874350@gmail.com>
2025-11-03 04:49:33 +0100

    Update playlist.tsx

--- playlist.tsx
+++ playlist.tsx
@@ -1,1 +1,11 @@
-// Playlist-Code
+import React from 'react';
+import { View, Text, FlatList } from 'react-native';
+export function PlaylistScreen() {
+  const [playlist, setPlaylist] = React.useState([]);
+  return (
+    <View>
+      <Text>Playlist Screen</Text>
+      <FlatList data={playlist} renderItem={({ item }) => <Text>{item}</Text>} />
+    </View>
+  );
+}
